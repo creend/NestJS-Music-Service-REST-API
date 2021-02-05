5c0ec0b0cd6fd941ab46588b6f10d9cacc5982a6
Creend <creend42@gmail.com>
2021-02-05 18:17:09 +0100

    Created user schema and Injected to users service and module

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export enum UserType {
+  Admin = 'admin',
+  Moderator = 'moderator',
+  Normal = 'normal',
+}
+
+const UserTypes: string[] = [
+  UserType.Admin,
+  UserType.Moderator,
+  UserType.Normal,
+];
+
+@Schema()
+export class User extends Document {
+  @Prop({ maxlength: 255, minlength: 2, required: true, unique: true })
+  username: string;
+
+  @Prop({ maxlength: 255, minlength: 2, required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
+
+  @Prop({ required: true, enum: UserTypes })
+  userType: UserType;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
