57fa1de3113c4e2c285ff05858061462b7e72ddc
Creend <creend42@gmail.com>
2021-02-05 14:26:13 +0100

    Changed name of User module and service to Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { MusicModule } from './music/music.module';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,7 +15,7 @@ import { UserModule } from './user/user.module';
     }),
     MusicModule,
     AuthModule,
-    UserModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
