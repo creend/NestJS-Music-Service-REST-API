c3522e1f0332af1b2ea2d6e428eb5b9588125bfb
Creend <creend42@gmail.com>
2021-02-05 18:26:30 +0100

    Created findOne method in users service, imported usersModule to AuthModule and inject UsersService to AuthService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/user.schema';
@@ -6,4 +6,12 @@ import { User } from 'src/schemas/user.schema';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  async findOne(email: string): Promise<User> {
+    // const user = this.userModel.findOne({ email });
+    // if (!user) {
+    //   throw new NotFoundException('User is not found');
+    // }
+    // return user;
+    return this.userModel.findOne({ email });
+  }
 }
