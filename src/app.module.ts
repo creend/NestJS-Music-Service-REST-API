0b5a82c1d3059d1a70d7489d47bc6a5836863cb9
Creend <creend42@gmail.com>
2021-02-05 14:15:12 +0100

    Generated Modules: User, Auth, Services: User, Auth and imported to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { MusicModule } from './music/music.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { MusicModule } from './music/music.module';
       useFindAndModify: false,
     }),
     MusicModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
