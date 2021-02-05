b5fc82a63377a0d2f56e7157d986886d59c3441a
Creend <creend42@gmail.com>
2021-02-05 19:10:34 +0100

    Created AuthController, login, register methods, create-user dto and installed bcryptjs and @types/bcryptjs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/schemas/user.schema';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(@Inject(UsersService) private usersService: UsersService) {}
+  @UseGuards(AuthGuard('local'))
+  @Post('/login')
+  async login(@Request() req: any) {
+    return req.user;
+  }
+
+  @Post('/register')
+  @HttpCode(201)
+  async register(@Body() user: CreateUserDto): Promise<User> {
+    return this.usersService.registerUser(user);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadGatewayException,
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcryptjs';
 import { Model } from 'mongoose';
-import { User } from 'src/schemas/user.schema';
+import { CreateUserDto } from 'src/auth/dto/create-user.dto';
+import { User, UserType } from 'src/schemas/user.schema';
 
 @Injectable()
 export class UsersService {
@@ -13,5 +21,31 @@ export class UsersService {
     // }
     // return user;
     return this.userModel.findOne({ email });
+  }
+  async registerUser(user: CreateUserDto): Promise<User> {
+    const { username, email, password, retypedPassword } = user;
+    const emailExist = await this.userModel.findOne({ email }).exec();
+    const usernameExist = await this.userModel.findOne({ username }).exec();
+
+    if (password !== retypedPassword) {
+      throw new BadRequestException('Passwords are not identical');
+    }
+
+    if (emailExist) {
+      throw new BadRequestException('Account with this email alredy exist');
+    }
+    if (usernameExist) {
+      throw new BadRequestException('Account with this username alredy exist');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+    const createdUser = await this.userModel.create({
+      username,
+      userType: UserType.Normal,
+      passwordHash,
+      email,
+    });
+    await createdUser.save();
+    return createdUser;
   }
 }
